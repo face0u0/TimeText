de034eb98524316768e35f5ca43dfc6a37923911
Hiroki <face9363>
2019-10-26 01:11:00 +0900

    first commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button v-on:click="$emit('name', username)"></button>
+    <input v-model="username" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      username: "",
+      classdata: null
+    };
+  },
+  methods: {
+    pushname: function() {
+      this.$axios
+        .get(
+          "https://util-api-face.herokuapp.com/me/all?user_name=" +
+            this.username
+        )
+        .then(function(response) {
+          this.classdata = response.data;
+        })
+        .catch(function(reason) {
+          console.log(reason.data);
+        });
+    }
+  }
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <div>
+      <h2>Vue.jsではじめるSPA</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  create: function () {
+    console.log('AAAAA')
+  }
+}
+</script>
