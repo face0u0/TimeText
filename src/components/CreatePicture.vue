48a384da1d184ae4e824c82b5ebee6e3ad665593
Hiroki <face9363>
2019-10-27 04:16:10 +0900

    いちじょうほう

--- src/components/CreatePicture.vue
+++ src/components/CreatePicture.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <b-button v-b-modal.modal-1 variant="warning" class="position-absolute">Picture</b-button>
+    <b-button v-b-modal.modal-1 variant="warning" class="position-absolute"
+      >Picture</b-button
+    >
     <b-modal id="modal-1" title="Add new Image" :hide-footer="true">
       <form>
         <input
@@ -20,7 +22,7 @@
         <button
           class="btn btn-primary mt-2"
           type="submit"
-          v-on:click="sendData"
+          v-on:click="getLocation"
         >
           submit
         </button>
@@ -44,55 +46,82 @@ export default {
         image_url: "",
         body: "",
         class_time: null,
-        block: false
+        lat: null,
+        loc: null,
+        alt: null
       }
     };
   },
   methods: {
-    setFile: function (e) {
-      this.image_file = e.target.files[0]
+    setFile: function(e) {
+      this.image_file = e.target.files[0];
     },
-    sendData: function() {
-      if (this.createRequest["block"] || !this.image_file) {
+    getLocation: function() {
+      if (!navigator.geolocation) {
+        alert("no use location data");
+      }
+      const options = {
+        enableHighAccuracy: true,
+        // timeout: 5000,
+        maximumAge: 0
+      };
+      navigator.geolocation.getCurrentPosition(
+        this.sendData,
+        this.sendData,
+        options
+      );
+    },
+    sendData: function(position) {
+      if (!this.image_file) {
         return;
       }
-      this.$root.$emit('bv::hide::modal', 'modal-1', '#btnShow')
-      this.createRequest["block"] = true;
+      this.$root.$emit("bv::hide::modal", "modal-1", "#btnShow");
       this.imgurData = new FormData();
       this.imgurData.append("image", this.image_file);
+      if('coords' in position) {
+        this.createRequest["lat"] = position.coords.latitude;
+        this.createRequest["lng"] = position.coords.longitude;
+        this.createRequest["alt"] = position.coords.altitude;
+      }
       const imgurAxios = axios.create({
-        baseURL: "https://api.imgur.com/3/image",
-      })
-      imgurAxios.defaults.headers.common['Authorization'] = `Client-ID d45fcc7ee858cf5`
-      imgurAxios.post('', this.imgurData).then(function (res) {
-        this.createRequest['image_url'] = res.data.data.link;
-        this.createRequest["class_time"] = Math.round(
-          new Date().getTime() / 1000
-        );
-        axios
-          .post(
-            "https://util-api-face.herokuapp.com/clspict/class_contents?user_name=" +
-            this.username,
-            this.createRequest
-          )
-          .then(
-            function(response) {
-              console.log(response);
-              this.createRequest = {
-                image_url: "",
-                body: "",
-                class_time: null,
-                block: false
-              };
-            }.bind(this)
-          )
-          .catch(function(reason) {
-            console.log(reason);
-          });
-
-      }.bind(this)).catch(function (err) {
-        console.log(err)
-      })
+        baseURL: "https://api.imgur.com/3/image"
+      });
+      imgurAxios.defaults.headers.common["Authorization"] = `Client-ID d45fcc7ee858cf5`;
+      imgurAxios
+        .post("", this.imgurData)
+        .then(
+          function(res) {
+            this.createRequest["image_url"] = res.data.data.link;
+            this.createRequest["class_time"] = Math.round(
+              new Date().getTime() / 1000
+            );
+            axios
+              .post(
+                "https://util-api-face.herokuapp.com/clspict/class_contents?user_name=" +
+                  this.username,
+                this.createRequest
+              )
+              .then(
+                function(response) {
+                  console.log(response);
+                  this.createRequest = {
+                    image_url: "",
+                    body: "",
+                    class_time: null,
+                    lat: 0,
+                    loc: 0,
+                    alt: 0
+                  };
+                }.bind(this)
+              )
+              .catch(function(reason) {
+                console.log(reason);
+              });
+          }.bind(this)
+        )
+        .catch(function(err) {
+          console.log(err);
+        });
     }
   }
 };
