032f5a26f2d36ef35670ac128497807aa234118e
Hiroki <face9363>
2019-10-28 12:07:06 +0900

    add firebase

--- src/main.js
+++ src/main.js
@@ -7,8 +7,25 @@ import router from './router'
 import axios from 'axios'
 import './assets/update.scss'
 import BootstrapVue from 'bootstrap-vue'
+import firebase from 'firebase/app'
+import "firebase/auth"
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBdvVF7_lciYklaUXWjjBJ3Q18jszJLX3E",
+  authDomain: "timetext-8a337.firebaseapp.com",
+  databaseURL: "https://timetext-8a337.firebaseio.com",
+  projectId: "timetext-8a337",
+  storageBucket: "timetext-8a337.appspot.com",
+  messagingSenderId: "1056224902579",
+  appId: "1:1056224902579:web:7fc8a44a7241159f89dfc6",
+  measurementId: "G-NDWRHR5DB0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 /* eslint-disable no-new */
 new Vue({
