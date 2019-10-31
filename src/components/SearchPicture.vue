faacad607c0ef21fb95755c9432bbd8429654e5c
face9363 <4181hiro@gmail.com>
2019-10-31 13:28:45 +0900

    change vue

---
+++ src/components/SearchPicture.vue
@@ -0,0 +1,111 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-8 offset-sm-1 col-md-6 offset-md-2 offset-lg-1 mb-3">
+                <b-input-group>
+                    <b-input-group-prepend is-text>
+                        <b-form-checkbox switch size="sm" class="mr-n2" v-model="checked">
+                            <span class="sr-only">around</span>
+                        </b-form-checkbox>
+                    </b-input-group-prepend>
+
+                    <b-form-select v-model="search">
+                        <option
+                        v-bind:key="i"
+                        v-for="(l, i) in classtimelist"
+                        v-bind:value="i"
+                        class="webfont"
+                        >
+                            {{ l }}
+                        </option>
+                    </b-form-select>
+                    <b-input-group-append>
+                        <b-button v-on:click="getPictureList" variant="outline-secondary">GET</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </div>
+        </div>
+        <h3 v-if="loading" class="webfont text-muted col-12 no-content">Loading...</h3>
+        <h3 v-else-if="!pictureList[0]" class="webfont text-muted col-12 no-content">no content</h3>
+        <div class="text-center" v-show="loading">
+            <b-spinner variant="secondary" label="Spinning" class="m-5"></b-spinner>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        props: ['username'],
+        name: "SearchPicture",
+        data: function() {
+            return {
+                pictureList: [],
+                search: 0,
+                checked: false,
+                loading: false
+            };
+        },
+        mounted: function () {
+            const l = localStorage.getItem('search');
+            if (l) {
+                this.search = l;
+                if(localStorage.name) {
+                    setTimeout(this.getPictureList, 1000);
+                }
+            }
+        },
+        watch: {
+            search: function () {
+                localStorage.setItem('search', this.search);
+            }
+        },
+        computed: {
+            classtimelist: function() {
+                const classlist = [];
+                for (let i = 0; i < 25; i++) {
+                    classlist.push(
+                        ["Mon", "Tus", "Wed", "Thu", "Fri"][Math.floor(i / 5)] +
+                        ((i % 5) + 1) +
+                        ""
+                    );
+                }
+                return classlist;
+            },
+        },
+        methods: {
+            getText: function(p){
+                return p.slice(-4)
+            },
+            getPictureList: function() {
+                this.loading = true;
+                this.pictureList = [];
+                let url;
+                if (this.checked){
+                    url = "/me/around/"+this.search+"?user_name="+this.username;
+                }
+                else {
+                    url = "/me/part/"+this.search+"?user_name="+this.username
+                }
+                axios
+                    .get(
+                        "https://util-api-face.herokuapp.com/clspict" + url
+                    )
+                    .then(
+                        function(response) {
+                            this.pictureList = response.data.reverse();
+                            this.$emit('picturelist', this.pictureList);
+                            console.log(this.pictureList);
+                        }.bind(this)
+                    )
+                    .catch(function(reason) {
+                        console.log(reason.response);
+                    })
+                    .finally(function () {
+                        this.loading = false
+                    }.bind(this));
+            }
+        }
+    };
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,9 @@
     <my-name v-on:name="username=$event"></my-name>
     <create-picture v-bind:username="username" class="my-3 offset-7 offset-sm-9 offset-md-10 offset-lg-11"></create-picture>
     <div>
-      <picture-list v-bind:username="username"></picture-list>
+      <search-picture v-on:picturelist="picturelist=$event"
+                      v-bind:username="username"></search-picture>
+      <picture-list v-bind:picturelist="picturelist"></picture-list>
     </div>
   </div>
 
@@ -14,11 +16,14 @@ import PictureList from '../components/PictureList'
 import CreatePicture from '../components/CreatePicture'
 import DarkHeader from '../components/DarkHeader'
 import MyName from '../components/MyName'
+import SearchPicture from "../components/SearchPicture";
+
 export default {
   name: 'Home',
-  components: {DarkHeader, CreatePicture, PictureList, MyName},
+  components: {DarkHeader, CreatePicture, PictureList, MyName, SearchPicture},
   data: function () {
     return {
+      picturelist: [],
       username: ""
     }
   }
